443fc72bd5cda02a6efc4469a2ec2ddf1e0e1e6b
Lucas Ângelo <dev.lucas.angelo@gmail.com>
2026-06-02 12:04:42 -0300

    ANDROID: Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "dev.lucasangelo.cgc1"
         minSdk = 24
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "v1.0rev0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
